964e26ff4d850c0f9290dec08537bc1403d4541c
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-09 18:38:53 +0700

    Update start-test.sh

--- start-test.sh
+++ start-test.sh
@@ -2,3 +2,6 @@ pip3 install requests pytest
 git clone https://github.com/bngoc1307/Nhom04_CCPTPM
 cd Nhom04_CCPTPM
 python3 -m pytest
+sudo pkill honcho
+sudo service mongod stop
+sudo service rabbitmq-server stop
